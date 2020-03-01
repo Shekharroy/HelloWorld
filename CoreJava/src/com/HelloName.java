a95ba137cd88c1253b07ca52925c906d12f50261
Chandrashekhar <Chandrashekhar@DESKTOP-Q378QTM>
2020-03-01 18:55:10 +0530

    Hello Name

---
+++ CoreJava/src/com/HelloName.java
@@ -0,0 +1,13 @@
+package com;//package name
+
+public class HelloName {// class name
+
+	public static void main(String[] args) {//main method of HelloName class
+		//first way the doing the program;
+		System.out.println("Hello\nChandrashekhar Kumar\n");
+		//second way to do;
+		System.out.println("Hello");
+		System.out.println("Chandrashekhar Kumar");
+	}
+
+}

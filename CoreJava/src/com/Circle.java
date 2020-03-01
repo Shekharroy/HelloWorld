bc7d2e3fc211fbbebd653ca2e808b295c421a29e
Chandrashekhar <Chandrashekhar@DESKTOP-Q378QTM>
2020-03-01 21:22:19 +0530

    Circle : Area and Perimeter

---
+++ CoreJava/src/com/Circle.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Circle {
+	public static void main(String arr[]) {
+		float radius = 7.5f;
+		System.out.println("Area of circle = "+(Math.PI*radius*radius));
+		System.out.println("Perimeter of circle = "+2*(Math.PI*radius));
+	}
+
+}

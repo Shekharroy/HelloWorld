e3e7422282512c1840a72184a9ef28625f445809
Chandrashekhar <Chandrashekhar@DESKTOP-Q378QTM>
2020-03-01 19:20:37 +0530

    Divide two number

---
+++ CoreJava/src/com/DivideTwoNumber.java
@@ -0,0 +1,23 @@
+package com;
+/**
+ * Write a Java program to divide two numbers and print on the screen
+ * Test Data :
+ * 50/3
+ * Expected Output :
+ * 16
+ **/
+ public class DivideTwoNumber {
+	 public static void main(String arr[]) {
+		 // variable;
+		 int a = 50, b = 3, r;
+		 r = a/b;
+		 System.out.println("Divide two number = "+r);
+		 
+		 // divide two number without using variable;
+		 System.out.println("Divide two number without using variable..");
+		 System.out.println(50/3);
+		 
+		 
+	 }
+
+}

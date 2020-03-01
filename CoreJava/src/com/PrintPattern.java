33713ea72a9ae81c69382237eb50a8422fca6374
Chandrashekhar <Chandrashekhar@DESKTOP-Q378QTM>
2020-03-01 20:51:25 +0530

    Pattern Print

---
+++ CoreJava/src/com/PrintPattern.java
@@ -0,0 +1,19 @@
+package com;
+/*
+ * Write a Java program to display the following pattern
+ * Sample Pattern :
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+ * */
+
+public class PrintPattern {
+	public static void main(String arr[]) {
+		System.out.println("   J    a   v     v  a");
+		System.out.println("   J   a a   v   v  a a ");
+		System.out.println("J  J  aaaaa   V V  aaaaa");
+		System.out.println(" JJ  a     a   V  a     a");
+	}
+}
